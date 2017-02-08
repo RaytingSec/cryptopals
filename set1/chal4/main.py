5f89c82933f50445df1791f78769fea0c09eef4c
raydarnet <hansen.wu.85@gmail.com>
2017-02-07 17:20:16 -0800

    simple frequency analysis

--- set1/chal4/main.py
+++ set1/chal4/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Detect single byte XOR
+import collections
 
 
 def charxor(string, char):
@@ -7,16 +8,32 @@ def charxor(string, char):
     return b''.join(xored)
 
 
+def freqtest(string):
+    commonletters = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'u']
+    commonbytes = []
+    for c in commonletters:
+        commonbytes.append(str.encode(c))
+
+    common = collections.Counter(string.replace(' ', '')).most_common()[0][0]
+    if common in commonletters:
+        return True
+    else:
+        return False
+
+
 with open('4.txt') as f:
-    ciphertexts = f.read().split('\n')
+    ciphertexts = map(bytes.fromhex, f.read().split('\n'))
 
 for text in ciphertexts:
-    textbytes = bytes.fromhex(text)
-    for x in range(0, 26):
-        char = chr(65 + x)
+    # textbytes = bytes.fromhex(text)
+    # print('text: ' + text)
+    for x in range(0, 255):
+        char = chr(x)
         xored = charxor(textbytes, char)
         try:
-            print(char, str(xored, 'utf-8').encode('utf-8'))
+            xored = str(xored, 'utf-8')
+            if freqtest(xored):
+                print('{}: {}'.format(char, xored))
         except UnicodeDecodeError:
             # print("failed")
             pass
